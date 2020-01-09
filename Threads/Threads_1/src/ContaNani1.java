65084e1212e0ed278e332607cd6e2a0bc42e42dc
Carlo Contardi <c.contardi@icloud.com>
2020-01-09 09:34:05 +0100

    Update ContaNani1.java
    change the range of count

--- Threads/Threads_1/src/ContaNani1.java
+++ Threads/Threads_1/src/ContaNani1.java
@@ -2,7 +2,7 @@ public class ContaNani1 extends Thread {
     public void run(){
         setName("Sette Nani");
         System.out.println(Thread.currentThread().getName());
-        for(int i=0;i<7;i++){
+        for(int i=0;i<27;i++){
             System.out.println((i+1)+ " ");
         }
     }
